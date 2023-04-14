6187255a2254ae3a6e682d0a8661baaeb33e3d29
tanasov49 <tanasov49@gmail.com>
2023-04-14 11:19:36 +0200

    for github

--- src/components/UI/Logo/Logo.tsx
+++ src/components/UI/Logo/Logo.tsx
@@ -3,12 +3,13 @@ import LogoImage from '../../../../public/images/logo.png'
 import Image from 'next/image';
 import styles from './Logo.module.scss'
 import { ModalContext } from '@/context/context';
+import Link from 'next/link';
 function Logo() {
     const {isOpenMenu} = useContext(ModalContext)
     return (
-        <a href='/' className={isOpenMenu ? `${styles.logo} ${styles['logo_active']}` : `${styles.logo}`}> 
+        <Link href='/' className={isOpenMenu ? `${styles.logo} ${styles['logo_active']}` : `${styles.logo}`}> 
             <Image src={LogoImage} alt='logo' />
-        </a>
+        </Link>
     );
 }
 
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,16 @@
 import React, {useEffect} from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 function NotFoundPage() {
     const router = useRouter();
-    useEffect(() => {
-        setTimeout(() => {
+    useEffect(() => {setTimeout(() => {
             router.push('/');
         }, 3000);
     },[]);
     return (
         <div className='not-found'>
             <h2 className='not-found__title'>This page not found</h2>
+            <Link href={'/'}>Home</Link>
         </div>
     );
 }
